4c7d58d7d3ad5fd7398325bccb6e40807b0a4727
zhengzw <zhengxuanhz@163.com>
2017-02-16 17:33:02 +0800

    文件读写完成

--- PointCloudFusion/mainwindow.h
+++ PointCloudFusion/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "zPCFWidget.h"
 
 namespace Ui {
 class MainWindow;
@@ -20,6 +21,7 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+//    zPCFWidget *pcfWidget;
 };
 
 #endif // MAINWINDOW_H
